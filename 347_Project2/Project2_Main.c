f4d7cc2338a518f46b2e880c7bfe6a504ca90693
Jose Melgar <jose.melgar.u@gmail.com>
2018-11-07 19:38:03 -0800

    Demo version

--- 347_Project2/Project2_Main.c
+++ 347_Project2/Project2_Main.c
@@ -34,7 +34,7 @@ unsigned char pressed = 0;
 unsigned char released = 1;
 
 ///////////////////ADC Stuff///////////////////
-int adcTable[] = {4095, 3050, 1980, 1370, 950, 860, 730, 650, 570, 530, 460, 390, 330, 300, 0};
+int adcTable[] = {4095, 3050, 1980, 1370, 950, 830, 730, 650, 570, 530, 460, 390, 330, 300, 0};
 int distTable[] = {0, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 999};
 float distance = 0;
 float calibration = 0;
